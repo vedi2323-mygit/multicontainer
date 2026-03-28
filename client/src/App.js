a814a86c2a7d85b063d7c96452308b2cdf3ef75e
VEDI23 <vedims23@VediMS.localdomain>
2026-03-28 07:16:58 +0000

    changed the name

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fibonacci calculator  
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
