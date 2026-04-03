d724fc4207c9718cb734d75dcaf293fed2bf2986
VEDI23 <vedims23@VediMS.localdomain>
2026-04-03 17:15:44 +0000

    removed ssl

--- server/index.js
+++ server/index.js
@@ -17,10 +17,6 @@ const pgClient = new Pool({
   database: keys.pgDatabase,
   password: keys.pgPassword,
   port: keys.pgPort,
-  ssl:
-    process.env.NODE_ENV !== 'production'
-      ? false
-      : { rejectUnauthorized: false },
 });
 
 pgClient.on('connect', (client) => {
